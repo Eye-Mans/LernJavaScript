a445f7f5d1c6143f3e0f6711e07b6df49a640f8c
Eye-Mans <laluiman468@gmail.com>
2024-01-29 10:38:15 +0700

    first commit

---
+++ JS const/js.js
@@ -0,0 +1,16 @@
+
+let i = 3;
+console.log(i)
+{
+    let i = 3;
+}
+
+var a = 10;
+{
+    var a =6;
+}
+console.log(a)
+
+let z = 3;
+z = 7;
+console.log(z)
---
+++ JS variabel/JS.js
@@ -0,0 +1,12 @@
+// variabel Global
+firstName = "roy"
+
+console.log(firstName)
+
+let middleName="iman", 
+    carName="lambo",
+    umur=16;
+
+console.log(carName)
+
+var i = 7 + 5
